4c305e3f0be9905d07cc26a41b8f366af5216fd9
Ahmad Fakhry <ahmad.fakhry@mail.utoronto.ca>
2021-02-12 17:25:41 -0500

    Register changes with functionality

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
+  // username = '';
+  password = '';
+  email = '';
   constructor() { }
 
   ngOnInit(): void {
+    console.log("TEST");
+  }
+
+  test(): void {
+    // console.log("TEST");
+    try {
+      axios.post("https://testing-environment-300301.firebaseapp.com/newuser", {
+        email: this.email, password: this.password
+      });
+    } catch (error) {
+      console.log(error);
+    }
   }
 
 }
