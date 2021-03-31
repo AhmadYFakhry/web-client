33b81c0a0262f62823272608c646b25888cdace6
Ryan Antiochos <rantioch@uoguelph.ca>
2021-03-31 13:36:01 -0400

    Changes March 31st

--- src/components/forms/reg-form/Registration.tsx
+++ src/components/forms/reg-form/Registration.tsx
@@ -11,7 +11,27 @@ import {
   Loading,
 } from './reg-steps/index';
 import NewUserForm from '../newuser-form/newuser-form';
+import firebase from 'firebase';
+
 const backendURL = 'https://testing-environment-300301.firebaseapp.com/';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAxfuwSd2lUeKjcoKSATMSCmSMCmS2nSNM",
+  authDomain: "testing-environment-300301.firebaseapp.com",
+  projectId: "testing-environment-300301",
+  storageBucket: "testing-environment-300301.appspot.com",
+  messagingSenderId: "274541959464",
+  appId: "1:274541959464:web:60368958ab8ef34b86581d",
+  measurementId: "G-XNZXDQYBK3"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize FireStore Database
+var db = firebase.firestore();
+
 
 const Register = () => {
   // const { register, handleSubmit } = useForm<RegistrationFormInput>();
@@ -27,6 +47,32 @@ const Register = () => {
   const onSubmit = async (data: any) => {
     const newUser = { ...data, name: `${data.firstName}, ${data.lastName}` };
     try {
+      firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+      .then((userCredential: any) => { 
+
+          //Create user object
+          var user = userCredential.user;
+
+          //Inputting User Preferences and Types into the Database
+          db.collection("Users").doc(user.uid).set({
+            type: ""
+          }).then(function() { 
+            console.log("Database Update Successful");
+          }).catch(function(error: any) { 
+            console.error("Error Rewriting to Document", error);
+          });
+
+          //Inputting User Information
+          user.updateProfile({
+            displayName: newUser.name
+          }).then(function() {
+            console.log("User Update Successful");
+          }).catch(function(error: any) {
+            console.error("User Update Error", error);
+          });
+          console.log("User Registration Successful");
+          })
+
       console.log(newUser);
       successToast();
     } catch (error) {
